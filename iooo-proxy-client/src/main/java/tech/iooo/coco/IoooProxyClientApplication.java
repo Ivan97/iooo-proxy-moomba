a375c09eecaeb9029bf755182affd5de769fdc69
YangkiZhang <yangkizhang@gmail.com>
2018-07-11 16:39:50 +0800

    [Initial]:[initial]

---
+++ iooo-proxy-client/src/main/java/tech/iooo/coco/IoooProxyClientApplication.java
@@ -0,0 +1,15 @@
+package tech.iooo.coco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ivan97
+ */
+@SpringBootApplication
+public class IoooProxyClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IoooProxyClientApplication.class, args);
+  }
+}
---
+++ iooo-proxy-server/src/main/java/tech/iooo/coco/IoooProxyServerApplication.java
@@ -0,0 +1,15 @@
+package tech.iooo.coco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ivan97
+ */
+@SpringBootApplication
+public class IoooProxyServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IoooProxyServerApplication.class, args);
+  }
+}
