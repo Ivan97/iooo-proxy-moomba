cbc3e5cfa0660a6c4b71f01709fc51dcb47ad82c
YangkiZhang <yangkizhang@gmail.com>
2018-07-12 16:47:13 +0800

    [TEST]:[test case]

--- iooo-proxy-server/src/test/java/tech/iooo/coco/test/HttpProxyClient.java
+++ iooo-proxy-server/src/test/java/tech/iooo/coco/test/HttpProxyClient.java
@@ -1,42 +1,60 @@
 package tech.iooo.coco.test;
 
-import java.net.InetSocketAddress;
-import java.net.PasswordAuthentication;
-import java.net.Proxy;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import io.vertx.core.Vertx;
+import io.vertx.core.net.ProxyOptions;
+import io.vertx.core.net.ProxyType;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import java.util.concurrent.CountDownLatch;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RunWith(JUnit4.class)
 public class HttpProxyClient {
 
+  private static final Logger logger = LoggerFactory.getLogger(HttpProxyClient.class);
+
+  private int i = 10;
+  private CountDownLatch latch = new CountDownLatch(i);
+
   @Test
-  public void test() throws Exception {
-    final String user = "ivan97";
-    final String password = "test";
-
-    Proxy proxyTest = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 1087));
-
-    java.net.Authenticator.setDefault(new java.net.Authenticator() {
-      private PasswordAuthentication authentication = new PasswordAuthentication(user, password.toCharArray());
-
-      @Override
-      protected PasswordAuthentication getPasswordAuthentication() {
-        return authentication;
-      }
-    });
-
-    OkHttpClient client = new OkHttpClient.Builder().proxy(proxyTest).build();
-    Request request = new Request.Builder().url("https://www.baidu.com").build();
-    Response response = client.newCall(request).execute();
-    System.out.println(response.code());
-    System.out.println(response.body().string());
-
-    client.dispatcher().executorService().shutdown();
-    client.connectionPool().evictAll();
+  public void test() {
+    for (int i = 0; i < this.i - 1; i++) {
+      new Thread(new Runner()).start();
+    }
+    try {
+      latch.await();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
+  class Runner implements Runnable {
+
+    @Override
+    public void run() {
+      Vertx vertx = Vertx.vertx();
+      WebClient client = WebClient.create(vertx,
+          new WebClientOptions().setProxyOptions(
+              new ProxyOptions()
+                  .setType(ProxyType.SOCKS5)
+                  .setHost("127.0.0.1")
+                  .setPort(1089)
+                  .setUsername("ivan97")
+                  .setPassword("test")));
+      logger.info("called...");
+      client.get("https://www.baidu.com").send(result -> {
+        if (result.succeeded()) {
+          logger.info("statusCode:{}", result.result().statusCode());
+          latch.countDown();
+        } else {
+          logger.error("error", result.cause());
+          latch.countDown();
+        }
+      });
+    }
+  }
 }
